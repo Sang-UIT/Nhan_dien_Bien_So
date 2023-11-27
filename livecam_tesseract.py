761d135014047c98c567d5de12fcca589a586b54
Sang-UIT <20520736@gm.uit.edu.vn>
2023-11-27 17:11:33 +0700

    add_new_tesseract

--- livecam_tesseract.py
+++ livecam_tesseract.py
@@ -4,12 +4,14 @@ import torch
 import function.utils_rotate as utils_rotate
 import function.helper as helper
 import av
+import pytesseract
+import streamlit as st
 # load model
 yolo_LP_detect = torch.hub.load('yolov5', 'custom', path='model/LP_detector_nano_61.pt', force_reload=True, source='local')
 yolo_LP_detect.conf=0.80
 yolo_license_plate = torch.hub.load('yolov5', 'custom', path='model/LP_ocr_nano_62.pt', force_reload=True, source='local')
 yolo_license_plate.conf = 0.60
-
+pytesseract.pytesseract.tesseract_cmd = "D:\\Tesseract_OCR\\tesseract.exe"
 class Detect(VideoTransformerBase):
     def __init__(self):
         self.i = 0
@@ -18,26 +20,23 @@ class Detect(VideoTransformerBase):
         plates = yolo_LP_detect(img, size=640)
         list_plates = plates.pandas().xyxy[0].values.tolist()
         list_read_plates = set()
+        
         for plate in list_plates:
-            flag = 0
             x = int(plate[0]) # xmin
             y = int(plate[1]) # ymin
             w = int(plate[2] - plate[0]) # xmax - xmin
             h = int(plate[3] - plate[1]) # ymax - ymin  
             crop_img = img[y:y+h, x:x+w]
+            cv2.imwrite("crop.jpg",crop_img)
             cv2.rectangle(img, (int(plate[0]),int(plate[1])), (int(plate[2]),int(plate[3])), color = (0,0,225), thickness = 2)
-            cv2.imwrite("crop.jpg", crop_img)
-            rc_image = cv2.imread("crop.jpg")
             lp = ""
-            for cc in range(0,2):
-                for ct in range(0,2):
-                    lp = helper.read_plate(yolo_license_plate, utils_rotate.deskew(rc_image, cc, ct))
-                    if lp != "unknown":
-                        list_read_plates.add(lp)
-                        cv2.putText(img, lp, (int(plate[0]), int(plate[1]-10)), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
-                        flag = 1
-                        break
-            if flag == 1:
-                break  
-        
+            r_img= utils_rotate.deskew(crop_img, 2, 0)
+            cv2.imwrite("crop_rotate.jpg", r_img)
+            predicted_result = pytesseract.image_to_string(r_img,lang='eng')
+            print(predicted_result)
+            lp = "".join(predicted_result.split()).replace(":", "").replace("-", "") 
+            if lp != "unknown":
+                list_read_plates.add(lp)
+            cv2.putText(img, lp, (int(plate[0]), int(plate[1]-10)), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+               
         return img
