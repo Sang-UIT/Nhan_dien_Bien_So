41f868c64f3883b0607f623c4f36140b193beb4c
Sang-UIT <20520736@gm.uit.edu.vn>
2023-11-28 21:55:27 +0700

    add req

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import livecam
 import livecam_tesseract
 from streamlit_webrtc import webrtc_streamer
 
-#webrtc_streamer=webrtc_streamer(key="testing", video_transformer_factory=livecam.Detect)
-webrtc_streamer=webrtc_streamer(key="testing", video_transformer_factory=livecam_tesseract.Detect)
+webrtc_streamer=webrtc_streamer(key="testing", video_transformer_factory=livecam.Detect)
+#webrtc_streamer=webrtc_streamer(key="testing", video_transformer_factory=livecam_tesseract.Detect)
